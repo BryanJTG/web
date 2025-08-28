6f38ad11e1e0a5f04cd529aa7176551ae7c63d1b
BryanJTG <tgbryanjimmy@gmail.com>
2025-08-27 22:03:51 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,71 @@
+// Selecciona el botón de hamburguesa
+const btn = document.querySelector(".menu-toggle");
+
+// Selecciona el menú horizontal
+const menu = document.querySelector(".menu-horizontal");
+
+// Al hacer clic en el botón de hamburguesa
+btn.addEventListener("click", () => {
+  // Alterna (agrega o quita) la clase "active" en el menú
+  // Esto hace que se muestre o se oculte
+  menu.classList.toggle("active");
+
+  // (opcional) También puedes alternar una clase en el botón si quieres cambiar su apariencia
+  // btn.classList.toggle("open");
+});
+
+
+
+
+
+
+
+
+// VALIDACION DE FORMULARIO
+const form = document.querySelector('form[name="form"]');
+form.addEventListener("submit", (event) => {
+  const fnombres = form.elements["nombres"].value;
+  const fapellidos = form.elements["apellidos"].value;
+  const femail = form.elements["email"].value;
+  const ftelefono = form.elements["telefono"].value;
+
+  if (!fnombres || !fapellidos || !femail || !ftelefono) {
+    event.preventDefault();
+    alert("Por favor, complete todos los campos del formulario");
+  } else if (!validateEmail(femail)) {
+    event.preventDefault();
+    alert("Por favor, ingrese un correo valido");
+  } else {
+    const confirmation = confirm(
+      "Esta a punto de enviar el formulario, ¿Desea Continuar?"
+    );
+    if (!confirmation) {
+      event.preventDefault();
+    }
+  }
+});
+
+//CREAR FUNCION validateEmail(femail)
+function validateEmail(femail) {
+  const re = /^[a-zA-Z0-9._]+@[a-zA-Z]+\.[a-zA-Z]/;
+  return re.test(String(femail).toLowerCase());
+}
+
+/*
+// Evento al enviar el formulario de contacto
+document.getElementById("formularioContacto").addEventListener("submit", function (e) {
+  e.preventDefault(); // Evita el envío real del formulario
+
+  // Muestra el mensaje de confirmación
+  const mensaje = document.getElementById("mensajeEnviado");
+  mensaje.style.display = "block";
+
+  // Opcional: limpiar el formulario
+  this.reset();
+
+  // Ocultar mensaje después de 5 segundos
+  setTimeout(() => {
+    mensaje.style.display = "none";
+  }, 5000);
+});
+*/
